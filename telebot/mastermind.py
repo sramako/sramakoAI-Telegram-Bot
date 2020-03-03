323d0e13ad69f0317da7a17c43212d9e1f80db19
Sraman <sramakoo@gmail.com>
2020-03-03 18:55:38 +0530

    Update mastermind.py

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -10,7 +10,7 @@ def get_response(msg):
         try:
             DB_USER = 'ako'
             DB_PASSWORD = 'secret123'
-            db_client = pymongo.MongoClient('mongodb://' + DB_USER + ':' + DB_PASSWORD + '@ds060749.mlab.com:60749/sramako_qtest')
+            db_client = pymongo.MongoClient('mongodb://' + DB_USER + ':' + DB_PASSWORD + '@ds060749.mlab.com:60749/sramako_qtest?retryWrites=false')
             db = db_client["sramako_qtest"]
         except:
             return "DB Access FAILED."
