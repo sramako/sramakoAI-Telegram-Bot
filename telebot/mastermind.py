08a7216b43cc21be8bda35f3c85910eec14ea6ff
Sraman <sramakoo@gmail.com>
2020-03-03 15:27:04 +0530

    Handle Register

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -1,5 +1,11 @@
 def get_response(msg):
-    """
-    you can place your mastermind AI here
-    """
-    return "This is a faltu response"
+    msg = msg.split()
+    COMMAND = msg[0]
+    COMMAND = COMMAND.lower()
+    if COMMAND == 'register'
+        GROUP = ' '.join(msg[1:-1])
+        GROUP = GROUP.upper()
+        EMAIL = msg[-1]
+        EMAIL = EMAIL.lower()
+        return 'GROUP:'+GROUP+'|EMAIL:'+EMAIL
+    return "This message was not handled."
